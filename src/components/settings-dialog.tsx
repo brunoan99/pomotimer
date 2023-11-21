3be8c33b839da28474b5beea78ddb06b142afee3
Bruno Andrade <brunoan99@gmail.com>
2023-11-20 21:55:14 -0300

    feat: start using shadcn components to create settings dialog structure

---
+++ src/components/settings-dialog.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { ModeToggle } from "./theme-toggle"
+
+export function DialogDemo() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when youre done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Pomodoro
+            </Label>
+            <Input
+              id="focus"
+              defaultValue="25"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Short Break
+            </Label>
+            <Input
+              id="short"
+              defaultValue="5"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Long Break
+            </Label>
+            <Input
+              id="long"
+              defaultValue="15"
+              className="col-span-3"
+            />
+          </div>
+          <div className="flex flex-row-reverse w-100%">
+          <ModeToggle />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
---
+++ src/layouts/Home.tsx
@@ -0,0 +1,10 @@
+import { DialogDemo } from "@/components/settings-dialog"
+
+const Home = () => (
+  <div className="w-screen p-8 flex flex-row justify-between">
+    <h1>PomoTimer</h1>
+    <DialogDemo />
+  </div>
+)
+
+export { Home }
